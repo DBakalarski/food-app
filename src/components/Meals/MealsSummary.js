679fa77ab5ff70cc89d8ea9d79304ea1a66fdd9c
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2022-06-07 18:30:35 +0200

    add meals component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from "./MelasSummary.module.css";
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered to You</h2>
+      <p>Choose your favourite meal!</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
