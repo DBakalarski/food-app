79affca15f1c8d4a10b093a8a5f9f61226728bb7
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2022-06-30 10:36:21 +0200

    add button animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,16 +2,35 @@ import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import { useContext } from "react";
 import CartContext from "../../store/cart-context";
+import { useEffect, useState } from "react";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
   const contextData = useContext(CartContext);
 
   const numberOfCartItems = contextData.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighLighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (contextData.items.length === 0) {
+      return;
+    }
+    setBtnIsHighLighted(true);
+
+    const timer = setTimeout(() => setBtnIsHighLighted(false), 300);
+    return () => {
+      console.log("clean");
+      clearTimeout(timer);
+    };
+  }, [contextData.items]);
+
   return (
-    <button onClick={props.onClick} className={classes.button}>
+    <button onClick={props.onClick} className={btnClasses}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
